5ff805d0c19e17a9e6eab8e5df43372c4d562bb2
DENNISINNOCENT <mutindahdennis@gmail.com>
2022-06-08 23:50:39 +0300

    commit tree 67264e1e63d0f6adaf9179a3c7d03db2bb917932

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+function returnFirstTwoDrivers(){
+    return drivers.slice(0,2)
+}
+function returnLastTwoDrivers(){
+    return drivers.slice(-2)
+}
+
+ const selectingDrivers=[returnFirstTwoDrivers,returnLastTwoDrivers]
+
+function  createFareMultiplier(){
+  return function multiplier(x){return x*x}
+}
+function   fareDoubler(doubleFares){
+    return doubleFares*2
+
+}
+function  fareTripler(trippleFares){
+    return trippleFares*3
+
+}
+function selectDifferentDrivers(drivers, returnDrivers){
+    return returnDrivers(drivers)
+
+}
